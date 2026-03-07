ba5f80d6c6cd1a39ba62746590a457136f295523
arthur <arthur.wuhrmann@epfl.ch>
2026-03-07 15:58:50 +0100

    fix: corrected the optimization direction

--- garak/resources/beast/beast_attack.py
+++ garak/resources/beast/beast_attack.py
@@ -270,7 +270,7 @@ class BeastAttack:
             best_score: The best score
         """
         best_suffix = ""
-        best_score = float("inf")
+        best_score = -float("inf")
 
         beams = [[sample] for sample in self._sample_tokens(prompt, k1, suffix_ids)]
         for i in tqdm(range(suffix_len), leave=False):
@@ -282,13 +282,13 @@ class BeastAttack:
                 self._score_candidates(prompt, response, candidate)
                 for candidate in candidates
             ]
-            sorted_scores = sorted(range(len(scores)), key=lambda j: scores[j])
+            sorted_scores = sorted(range(len(scores)), key=lambda j: scores[j], reverse=True)
             beams = [candidates[j] for j in sorted_scores[:k1]]
 
             best_candidate = candidates[sorted_scores[0]]
             candidate_score = scores[sorted_scores[0]]
 
-            if candidate_score < best_score:
+            if candidate_score > best_score:
                 best_score = candidate_score
                 best_suffix = best_candidate
 
