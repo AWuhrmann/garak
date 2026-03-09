4de6c5cd9ba4f4309d86b8dea0813ad8c80980d3
arthur <arthur.wuhrmann@epfl.ch>
2026-03-09 16:48:46 +0100

    added logging

--- garak/resources/beast/beast_attack.py
+++ garak/resources/beast/beast_attack.py
@@ -292,6 +292,7 @@ class BeastAttack:
                 best_score = candidate_score
                 best_suffix = best_candidate
             print(best_score)
+            print()
 
             if stop_early:
                 success = self._evaluate(prompt, best_candidate)
